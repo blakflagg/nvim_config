8139f22bc6a2e7b48469b3d2e75a06914c06b7f1
Dan - Win11 <locus78@gmail.com>
2022-11-13 20:18:10 -0800

    fixed TreeToggle

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -25,7 +25,6 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
-keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
@@ -71,3 +70,6 @@ keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 -- keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
 keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
 keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
+
+--NvimTree
+keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
